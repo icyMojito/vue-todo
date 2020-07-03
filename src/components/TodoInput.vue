d230e8068be5955f3f187d59066057454d7a92b2
coollime <dev.icymojito@gmail.com>
2020-07-03 19:51:29 +0900

    feat: TodoInput 컴포넌트 꾸미기
    - 입력창과 버튼 추가
    - 입력받은 텍스트를 로컬 스토리지에 저장 처리

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,10 +1,58 @@
 <template>
-  <div>input</div>
+  <div class="inputBox shadow">
+    <input type="text" v-model="newTodoItem" placeholder="할 일을 적어주세요!"
+    v-on:keyup.enter="addTodo">
+    <span class="addContainer" v-on:click="addTodo">
+      <i class="addBtn fas fa-plus" aria-hidden="true"></i>
+    </span>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      newTodoItem: ''
+    }
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodoItem !== '') {
+        const value = this.newTodoItem && this.newTodoItem.trim();
+        localStorage.setItem(value, value);
+        this.clearInput();
+      }
+    },
+    clearInput() {
+      this.newTodoItem = '';
+    }
+  }
+}
 </script>
 
 <style>
+  input:focus {
+    outline: none;
+  }
+  .inputBox {
+    background: white;
+    height: 50px;
+    line-height: 50px;
+    border-radius: 5px;
+  }
+  .inputBox input {
+    border-style: none;
+    font-size: 0.9rem;
+  }
+  .addContainer {
+    float: right;
+    background: linear-gradient(to right, #6478Fb, #8763FB);
+    display: block;
+    width: 3rem;
+    border-radius: 0 5px 5px 0;
+  }
+  .addBtn {
+    color: white;
+    vertical-align: middle;
+  }
 </style>
